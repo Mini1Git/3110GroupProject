a41e6aa3ffd4e1634d451faf0426e9944cf8c1c1
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-11-04 21:42:48 -0500

    constructors created

--- MyReader.java
+++ MyReader.java
@@ -1,3 +1,33 @@
+
+
 public class MyReader {
+    private String filename;//filename attribute
+    // default constructor
+    public MyReader() {
+
+    }
+
+    public MyReader(String filename) {
+      this.filename = filename; // seting the filename
+    }
     
+    
+  //   public static void main(String[] args) {
+  //   File myObj = new File("filename.txt");
+
+  //   // try-with-resources: Scanner will be closed automatically
+  //   try (Scanner myReader = new Scanner(myObj)) {
+  //     while (myReader.hasNextLine()) {
+  //       String data = myReader.nextLine();
+  //       System.out.println(data);
+  //     }
+  //   } catch (FileNotFoundException e) {
+  //     System.out.println("An error occurred.");
+  //     e.printStackTrace();
+  //   }
+  // }
 }
+
+
+
+
