02357eb457a14a58e5aeb1901d9f2914dae99c29
Daniel Lien <liend@uwindsor.ca>
2025-11-05 12:01:48 -0500

    Coded compare function in the LineComparator class

--- LineComparator.java
+++ LineComparator.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+//Line Comparator class
+
 public class LineComparator {
-    
+
+    //compare function
+    //accepts two arraylist. Each arrayList represents the lines form each file to compare
+    //compares, stores and returns results in an arraylist of pairs of integers (stored as int[])
+    public static ArrayList<int[]> compare(ArrayList<String> file1, ArrayList<String> file2){
+        //creates the result arraylist
+        ArrayList<int[]> results = new ArrayList<int[]>();
+
+        //a nested for loop to comapre each line from each file to check for matching string contents
+        for(int i = 0; i < file1.size(); i++){
+            for(int j = 0; j < file2.size(); j++){
+
+                if(file1.get(i).equals(file2.get(j))){
+                    //if the string content from file 1 and 2 match, add the line numbers to the results
+                    results.add(new int[]{i, j});
+                }
+
+            }
+        }
+
+        //return the matches
+        return results;
+    }
+
 }
 
