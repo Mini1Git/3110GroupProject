cff1f6abe6146b4efb510a73a9a17d76254fe8fa
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-11-05 13:01:25 -0500

    some testing

--- Main.java
+++ Main.java
@@ -12,11 +12,11 @@ public class Main {
         ArrayList<String> fileList1 = file1.listConverter();
         // System.out.println(fileList1);
 
-        int listSize = fileList1.size(); //size of arraylist
-        for(int i = 0; i<listSize; i++)
-        {
-            System.out.println(fileList1.get(i));
-        }
+        // int listSize = fileList1.size(); //size of arraylist
+        // for(int i = 0; i<listSize; i++)
+        // {
+        //     System.out.println(fileList1.get(i));
+        // }
 
         
         String fileName2 = input.nextLine(); //taking user input for file name
@@ -31,10 +31,19 @@ public class Main {
         // }
 
         // Print keys and values
-        System.out.println(fileHash);
+        // System.out.println(fileHash);
         
-        for (Integer i : fileHash.keySet()) {
-        System.out.println("key: "+ i + " value: " + fileHash.get(i));
+        // for (Integer i : fileHash.keySet()) {
+        // System.out.println("key: "+ i + " value: " + fileHash.get(i));
+        // }
+
+         int listSize = fileList1.size(); //size of arraylist
+        for(int i = 0; i<listSize; i++)
+        {
+             String temp = fileList1.get(i);
+             if(fileHash.containsValue(temp))
+              System.out.println(temp);
+             
         }
 
         input.close();
