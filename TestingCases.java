e88e7e1b551454f282527e1167cf812f9a082f83
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-11-06 15:50:13 -0500

    test cases changed due to index change

--- TestingCases.java
+++ TestingCases.java
@@ -17,10 +17,10 @@ public class TestingCases {
         //actual result should be [{0,0}, {1,1}, {3,5}, {4,3}] // one name got removed.
         ArrayList<int[]> expectedResults = new ArrayList<int[]>();
         //matches
-        int[] matchWilson = new int[]{0,0};
-        int[] matchLiana = new int[]{1,1};
-        int[] matchDaniel = new int[]{3,5};
-        int[] matchTyler = new int[]{4,3};
+        int[] matchWilson = new int[]{1,1};
+        int[] matchLiana = new int[]{2,2};
+        int[] matchDaniel = new int[]{4,6};
+        int[] matchTyler = new int[]{5,4};
 
         expectedResults.add(matchWilson);
         expectedResults.add(matchLiana);
