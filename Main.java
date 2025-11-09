4fdf85cc000f9a27c3b42b8d33b9020e0c15271c
Tyler Wang <wang5m2@uwindsor.ca>
2025-11-09 01:34:40 -0500

    updated main to newest ver

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args)
@@ -12,11 +12,11 @@ public class Main {
         ArrayList<String> fileList1 = file1.listConverter();
         // System.out.println(fileList1);
 
-        int listSize = fileList1.size(); //size of arraylist
-        for(int i = 0; i<listSize; i++)
-        {
-            System.out.println(fileList1.get(i));
-        }
+        // int listSize = fileList1.size(); //size of arraylist
+        // for(int i = 0; i<listSize; i++)
+        // {
+        //     System.out.println(fileList1.get(i));
+        // }
 
         
         String fileName2 = input.nextLine(); //taking user input for file name
@@ -31,11 +31,24 @@ public class Main {
         // }
 
         // Print keys and values
-        System.out.println(fileHash);
+        // System.out.println(fileHash);
         
-        for (Integer i : fileHash.keySet()) {
-        System.out.println("key: "+ i + " value: " + fileHash.get(i));
+        // for (Integer i : fileHash.keySet()) {
+        // System.out.println("key: "+ i + " value: " + fileHash.get(i));
+        // }
+
+         int listSize = fileList1.size(); //size of arraylist
+        for(int i = 0; i<listSize; i++)
+        {
+             String temp = fileList1.get(i);
+             if(fileHash.containsValue(temp))
+              System.out.println(temp);
+             
         }
+        /*
+        output here
+        writeToFile();
+        */
 
         input.close();
     }
