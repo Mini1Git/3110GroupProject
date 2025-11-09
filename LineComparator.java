099e1e7fccdbdde90f272d5956642b35fe06e7e9
Tyler Wang <wang5m2@uwindsor.ca>
2025-11-09 01:30:27 -0500

    included LineComparator from main

--- LineComparator.java
+++ LineComparator.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+//Line Comparator class
+
 public class LineComparator {
-    
+
+    //compare function
+    //accepts two arraylist. Each arrayList represents the lines form each file to compare
+    //compares, stores and returns results in an arraylist of pairs of integers (stored as int[])
+    public static ArrayList<int[]> compare(ArrayList<String> file1, ArrayList<String> file2){
+        //creates the result arraylist
+        ArrayList<int[]> results = new ArrayList<int[]>();
+        
+        //puting them in a variable reduce time complexity
+        int file1_size = file1.size();
+        int file2_size = file2.size();
+        //a nested for loop to comapre each line from each file to check for matching string contents
+        for(int i = 1; i < file1_size; i++){
+            if(file1.get(i).trim().isEmpty())
+                continue; //skiping the empty strings
+            for(int j = 1; j < file2_size; j++){
+
+                if(file2.get(j).trim().isEmpty())
+                    continue; //skiping the empty strings
+                if(file1.get(i).equals(file2.get(j))){
+                    //if the string content from file 1 and 2 match, add the line numbers to the results
+                    results.add(new int[]{i, j});
+                }
+
+            }
+        }
+
+        //return the matches
+        return results;
+    }
+
 }
 
