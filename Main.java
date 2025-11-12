8fdc36c9cdc253077f9d606eaac54fe5bece8570
nazifatahsinturna <nazifatahsinturnabd@gmail.com>
2025-11-12 01:58:05 -0500

    removed the commented code from main and put it in a dummy file

--- Main.java
+++ Main.java
@@ -10,13 +10,6 @@ public class Main {
 
         MyReader file1 = new MyReader(fileName1);
         ArrayList<String> fileList1 = file1.listConverter();
-        // System.out.println(fileList1);
-
-        // int listSize = fileList1.size(); //size of arraylist
-        // for(int i = 0; i<listSize; i++)
-        // {
-        //     System.out.println(fileList1.get(i));
-        // }
 
         
         String fileName2 = input.nextLine(); //taking user input for file name
@@ -24,29 +17,6 @@ public class Main {
         MyReader file2 = new MyReader(fileName2);
 
         ArrayList<String> fileList2 = file2.listConverter();
-        //HashMap<Integer, String> fileHash = file2.fileHasher();
-        
-        // Print values
-        // for (String i : fileHash.values()) {
-        // System.out.println(i);
-        // }
-
-        // Print keys and values
-        // System.out.println(fileHash);
-        
-        // for (Integer i : fileHash.keySet()) {
-        // System.out.println("key: "+ i + " value: " + fileHash.get(i));
-        // }
-
-        /* comparison print test between arrayList & hash
-        int listSize = fileList1.size(); //size of arraylist
-        for(int i = 0; i<listSize; i++)
-        {
-             String temp = fileList1.get(i);
-             if(fileHash.containsValue(temp))
-              System.out.println(temp);
-        }
-        */
         
         //comparison
         ArrayList<int []> compResults = LineComparator.compare(fileList1, fileList2);
