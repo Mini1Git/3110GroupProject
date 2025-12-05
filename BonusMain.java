9b4a9b6b07947705db4d1935be0ecb06d41ce332
bell23332 <bell23@uwindsor.ca>
2025-12-05 16:56:04 -0500

    simple main for testing

--- BonusMain.java
+++ BonusMain.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class BonusMain {
     public static void main(String[] args) {
-        // bonus main
+        Scanner input = new Scanner(System.in);
+        //get repository path
+        System.out.println("Enter full path to the Git repositroy: ");
+        String repoPath = input.nextLine().trim();
+
+        //get file to analyze for bugs
+        System.out.println("Enter the file name: ");
+        String fileName = input.nextLine().trim();
+
+        try{
+            BonusBugFinder.analyze(repoPath, fileName);
+        }
+        catch (Exception e){
+            System.err.println("Error ");
+        }
+        input.close();
     }
 }
