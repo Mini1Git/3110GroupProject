13e50b4dec6105a29f38acabe447f3108e50d2eb
bell23332 <bell23@uwindsor.ca>
2025-12-08 17:43:08 -0500

    improved structure of mappings, better handling for line splits, deleted lines

--- src/mainClasses/Output.java
+++ src/mainClasses/Output.java
@@ -1,35 +1,66 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
+
 public class Output {
     //loops through arrayList filled w arrays of two ints (pair of ints)
     //prints results on filename file
 
     public static void writeToFile(String filename, ArrayList<int[]> pairs){
+
+        //put the line pairings into a treeMap that sorts by original lines ascending order
+        // key = orig value = new
+        //and line splits can be stored more clearly
+        Map<Integer, List<Integer>> pairTree = new TreeMap<>();
+        for (int[] pair: pairs){
+            int orig = pair[0];
+            int neww = pair[1];
+            //add to pairTree, orig line nums as the key, value an ArrayList storing all NEW line nums
+            pairTree.computeIfAbsent(orig, k -> new ArrayList<>()).add(neww);
+        }
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
-            int old_x = 0;
 
             writer.write("<VERSION NUMBER = \"" + 1 + "\" >");
             writer.newLine();
 
             //loop through each element in arrayList
-            for (int[] pair : pairs){
-                //store the pair of ints
-                int x = pair[0];
-                int y = pair[1];
+            for (Map.Entry<Integer, List<Integer>> entry: pairTree.entrySet()){
+                int x = entry.getKey();
+                List<Integer> newLines = entry.getValue();
 
-                //line splits
-                if (old_x == x){
-                    writer.write("\t<ALT NEW = \"" + y + "\" />");
+                //order NEW lines
+                Collections.sort(newLines);
+
+                //single line match
+                if (newLines.size() == 1){
+                    writer.write("    <LOCATION ORIG= \"" + x + "\" NEW= \"" + newLines.get(0) + "\" />");
+                    writer.newLine();
                 }
-
+                //no matches ( deleted)
+                else if (newLines.isEmpty()) {
+                    writer.write("    <LOCATION ORIG= \"" + x + "\" NEW=\"-1\" />");
+                    writer.newLine();
+                }
+                //line split match
+                //first match printed normally
                 else{
-                    writer.write("<LOCATION ORIG = \"" + x + "\" NEW = \"" + y + "\" />");
+                    writer.write("    <LOCATION ORIG=\"" + x + "\" NEW=\"" + newLines.get(0) + "\">");
+                    writer.newLine();
+
+                    //other line split matches printed in ALT tags
+                    for (int i = 1; i < newLines.size(); i++) {
+                        writer.write("        <ALT NEW=\"" + newLines.get(i) + "\"/>");
+                        writer.newLine();
+                    }
+
+                    //closing tag
+                    writer.write("</LOCATION>");
+                    writer.newLine();
                 }
-                writer.newLine();
-                old_x = x;
             }
+
+
             writer.write("</VERSION>");
             System.out.print("Results written successfully to " + filename);
         } catch (IOException e) {
